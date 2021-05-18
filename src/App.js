9937402c6f5a1531b1d9eb3f16c67bcf16e6d436
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-05-18 15:49:17 -0700

    import components to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TodosList from "./components/todos-list.component";
+import EditTodo from "./components/edit-todo.component";
+import CreateTodo from "./components/create-todo.component";
+
 
 class App extends Component {
   render() {
